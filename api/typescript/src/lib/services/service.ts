7098411750d44b9d6aaa0bcbeffc86cf7de9b2b3
galenmarchetti <galenmarchetti@gmail.com>
2022-04-29 17:31:32 -0500

    adding typescript bindings

--- api/typescript/src/lib/services/service.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2020 - present Kurtosis Technologies LLC.
- * All Rights Reserved.
- */
-
-/*
-The identifier used for services within the enclave.
-*/
-export type ServiceID = string;
